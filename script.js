55080870ed5e49f2000e8168e624028d613fe4e2
Jemes_Stepanov <h15789471@gmail.com>
2026-04-03 22:52:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyC3uqEIX7Zee5_T9NAp5impPdK2nCPYIFo",
-  authDomain: "admin-panel-site-c6658.firebaseapp.com",
-  projectId: "admin-panel-site-c6658",
-  storageBucket: "admin-panel-site-c6658.firebasestorage.app",
-  messagingSenderId: "1007133132643",
-  appId: "1:1007133132643:web:d8f203b6736fef5879b556",
-  measurementId: "G-GFPJ1MLCZ5"
+    apiKey: "AIzaSyC3uqEIX7Zee5_T9NAp5impPdK2nCPYIFo",
+    authDomain: "admin-panel-site-c6658.firebaseapp.com",
+    projectId: "admin-panel-site-c6658",
+    storageBucket: "admin-panel-site-c6658.firebasestorage.app",
+    messagingSenderId: "1007133132643",
+    appId: "1:1007133132643:web:d8f203b6736fef5879b556",
+    measurementId: "G-GFPJ1MLCZ5"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 const ADMIN_PASSWORD = "AdminPanel2025";
 const SERVER_CITY = "spb";
